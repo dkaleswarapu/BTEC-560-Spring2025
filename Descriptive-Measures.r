3c3cb54f76a9de4696861550bc075d300fbfbe80
dkaleswarapu <raokaleswarapu@gmail.com>
2025-01-28 10:12:12 -0500

    Add files via upload

---
+++ Descriptive-Measures.r
@@ -0,0 +1,33 @@
+
+#Author:Rao Kaleswarapu, Date:01/28/2025, Purpose: Sedcriptive Measures Functions
+
+# Calculate Standard Deviation
+
+data<-c(1,2,3,4,5,6,7,8)
+
+data<-c("A","B","C")
+
+sd(data)
+[1] 2.44949
+
+data2<-c(1:1000)
+#[1] 288.8194
+
+
+
+#Calculate Mean
+
+
+mean(data)
+#[1] 4.5
+#Calculate Median
+
+median(data
+#[1] 4.5
+
+
+#Calculate mode in R
+getmode<-function(v){
+
+
+}
